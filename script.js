ca16897e5621aabf10471fb9fa8104e32ca4754b
Raphael velasco <raphaelvelasco2@gmail.com>
2022-01-18 21:37:54 -0300

    Fiz o desafio

---
+++ script.js
@@ -0,0 +1,10 @@
+let name = "Raphael"
+let number = 17
+let year = 2021 - number
+console.log(year + number)
+console.log("Estou mexendo no console.log")
+let quartaFeira = true
+console.log(typeof quartaFeira, quartaFeira)
+
+
+
